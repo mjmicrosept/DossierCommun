63557749ae1801f1ede342e4151711941f93a4be
maratierj <maratier.j@gmail.com>
2018-12-03 16:44:19 +0100

    [Documents - Logs] - BugFix sur la génération des routes du controlleur

--- controllers/LogLaboDocumentsDeleteController.php
+++ controllers/LogLaboDocumentsDeleteController.php
@@ -5,7 +5,6 @@ namespace app\controllers;
 use Yii;
 use app\models\LogLaboDocumentsDelete;
 use app\models\LogLaboDocumentsDeleteSearch;
-use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 
